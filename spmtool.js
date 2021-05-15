710166115f3460a7d5008e558cb471159e3fa928
kabiroberai <oberai.kabir@gmail.com>
2021-05-16 03:35:43 +0530

    More cross platform stuff

--- spmtool.js
+++ spmtool.js
@@ -14,20 +14,23 @@ async function* getFiles(dir) {
     }
 }
 
-let libExt;
+let outputName;
 let ldFlags;
 switch (process.platform) {
     case "darwin":
-        libExt = "dylib";
+        outputName = p => `lib${p}.dylib`;
         ldFlags = p => `-L. -l${p} -rpath @loader_path/..`;
         break;
     case "linux":
-        libExt = "so";
+        outputName = p => `lib${p}.so`;
+        // I want a word with whoever decided it was a good idea to
+        // use the $ symbol in $ORIGIN
         ldFlags = p => 
             `-L. -Wl,--no-as-needed -l${p} -Wl,--as-needed -Wl,-rpath,\\'$$\\'ORIGIN/..`;
         break;
     case "win32":
-        libExt = "dll";
+        outputName = p => `${p}.dll`;
+        // TODO: Figure out ldFlags for Windows (do we need a .lib?)
         break;
     default:
         throw new Error("node-gyp + SPM is not yet supported on this platform.");
@@ -36,7 +39,7 @@ switch (process.platform) {
 const sourceExts = ["swift", "c", "cc", "cpp"];
 
 function outputPath(productName) {
-    return `./build/lib${productName}.${libExt}`;
+    return `./build/${outputName(productName)}`;
 }
 
 async function forceSymlink(target, path) {
@@ -63,7 +66,7 @@ async function forceSymlink(target, path) {
         const dir = spawnSync("swift", args).stdout.toString().trim();
         const output = outputPath(productName);
         await Promise.all([
-            forceSymlink(`${dir}/lib${productName}.${libExt}`, output),
+            forceSymlink(`${dir}/${outputName(productName)}`, output),
             forceSymlink(config, "build/curr")
         ]);
     } else if (mode === 'output') {
