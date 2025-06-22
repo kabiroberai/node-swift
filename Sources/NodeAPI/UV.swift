f406c06aaf1c8c76a1ae0933325f5a94fe32f295
Kabir Oberai <oberai.kabir@gmail.com>
2025-06-21 23:46:19 -0400

    Documentation

--- Sources/NodeAPI/UV.swift
+++ Sources/NodeAPI/UV.swift
@@ -9,11 +9,22 @@ public enum UV {
     }
 
     @MainActor private static func _setup() {
+        // By default, node takes over the main thread with an indefinite uv_run().
+        // This causes CFRunLoop sources to not be processed (also breaking GCD & MainActor)
+        // since the CFRunLoop never gets ticked. We instead need to flip things on their
+        // head: ie use CFRunLoop as the main driver of the process. This is feasible because
+        // libuv offers an API to "embed" its RunLoop into another. Specifically, it exposes
+        // a backend file descriptor & timer; we can tell GCD to watch these. Any time they
+        // trigger, we tick the uv loop once.
+
+        // References:
+        // https://github.com/TooTallNate/NodObjC/issues/2
         // https://github.com/electron/electron/blob/dac5e0cd1a8d31272f428d08289b4b66cb9192fc/shell/common/node_bindings.cc#L962
         // https://github.com/electron/electron/blob/dac5e0cd1a8d31272f428d08289b4b66cb9192fc/shell/common/node_bindings_mac.cc#L24
+        // https://github.com/indutny/node-cf/blob/de90092bb65bbdb6acbd0b00e18a360028b815f5/src/cf.cc
+        // [SpinEventLoopInternal]: https://github.com/nodejs/node/blob/11222f1a272b9b2ab000e75cbe3e09942bd2d877/src/api/embed_helpers.cc#L41
 
         let loop = uv_default_loop()
-
         let fd = uv_backend_fd(loop)
 
         let reader = DispatchSource.makeReadSource(fileDescriptor: fd, queue: .main)
@@ -32,32 +43,30 @@ public enum UV {
             }
         }
 
-        reader.setEventHandler {
-            wakeUpUV()
-        }
+        reader.setEventHandler { wakeUpUV() }
+        timer.setEventHandler { wakeUpUV() }
+        DispatchQueue.main.async { wakeUpUV() }
 
-        timer.setEventHandler {
-            wakeUpUV()
-        }
-
-        DispatchQueue.main.async {
-            wakeUpUV()
-        }
-
-        // TODO: figure out whether this is supported.
-        // specifically, RunLoop.main.run inside an async block,
-        // which services DispatchQueue.main.sync { uv_run() },
-        // means the above uv_run is called inside the node shell's
-        // uv_run. The docs say uv_run isn't re-entrant, whoops.
-        //
-        // Ideally, we'd 1) perform the above setup and
-        // 2) replace Node's main uv_run with a RunLoop.main.run()
+        // Now that we've set up the CF/GCD sources, we need to
+        // start the CFRunLoop. Ideally, we'd patch the Node.js
+        // source to 1) perform the above setup and 2) replace
+        // its uv_run with CFRunLoopRun: insertion point would
+        // be [SpinEventLoopInternal] linked above.
+        // However, the hacky alternative (while avoiding the need
+        // to patch Node) is to kick off the CFRunLoop on the next
+        // uv tick. This is hacky because we eventually end up with
+        // a callstack that looks like:
+        // uv_run -> [process uv_async_t] -> RunLoop.run -> wakeUpUV -> uv_run
+        // Note that uv_run is called re-entrantly here, which is
+        // explicitly unsupported per the documentation. This seems
+        // to work okay based on rudimentary testing but could definitely
+        // break in the future / under edge cases.
+        // TODO: figure out whether there's a better solution.
         let uvAsync = OpaquePointer(UnsafeMutableRawPointer.allocate(
             // for ABI stability, don't hardcode current uv_async_t size
             byteCount: uv_handle_size(UV_ASYNC),
             alignment: MemoryLayout<max_align_t>.alignment
         ))
-
         uv_async_init(loop, uvAsync) { _ in
             RunLoop.main.run()
         }
